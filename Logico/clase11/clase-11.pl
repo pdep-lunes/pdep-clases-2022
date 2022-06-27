cf7bcab10adbbc4cb720c2ecaae562087a6dcc5f
Julián Berbel Alt <julian@mumuki.org>
2022-06-27 12:36:40 -0300

    Agrego el código de la clase 11

--- Logico/clase11/clase-11.pl
+++ Logico/clase11/clase-11.pl
@@ -107,5 +107,136 @@ esObra(Obra) :-
     escribio(_, Obra).
 
 % Tarea: Una obra le gusta a Gus si es Sandman o la escribió Isaac Asimov. Tiene que ser inversible.
+
+% Alternativa 2
+leGustaAGus(sandman).
+
+leGustaAGus(Obra):-
+  escribio(isaacAsimov, Obra). 
+
+% Alternativa 3
+leGusta(gus, sandman).
+
+leGusta(gus, Obra) :-
+  escribio(isaacAsimov, Obra).
+
+
 % Tarea: Conviene contratar un artista si escribió un bestseller o es reincidente. Tiene que ser inversible.
+
+convieneContratar(Artista) :-
+  escribio(Artista, Obra),
+  esBestSeller(Obra).
+
+convieneContratar(Artista) :-
+  esReincidente(Artista).
+
 % Tarea: Queremos saber si una obra es rioplatense, que es cuando la nacionalidad de su artista es platense (Uruguay o Argentina). ¡Ojo con repetir lógica! 
+
+nacionalidad(elsaBornemann, argentina).
+nacionalidad(joseHernandez, argentina).
+nacionalidad(julioCortazar, argentina).
+nacionalidad(jorgeLuisBorges, argentina).
+nacionalidad(horacioQuiroga, uruguaya).
+nacionalidad(neilGaiman, britanica).
+nacionalidad(alanMoore, britanica).
+nacionalidad(warrenEllis, britanica).
+nacionalidad(terryPratchett, britanica).
+nacionalidad(brianAzarello, estadounidense).
+nacionalidad(frankMiller, estadounidense).
+nacionalidad(stephenKing, estadounidense).
+nacionalidad(isaacAsimov, rusa).
+
+esRioplatense(Obra) :-
+  escribio(Artista, Obra),
+  nacionalidad(Artista, Nacionalidad),
+  esNacionalidadPlatense(Nacionalidad).
+
+esNacionalidadPlatense(uruguaya).
+esNacionalidadPlatense(argentina).
+
+
+% Para todas las obras que escribió un artista, la obra es un comic
+
+soloEscribioComics(Artista) :-
+  escribio(Artista, _),
+  forall(escribio(Artista, Obra), esComic(Obra)).
+%        ^^^^^^^^^^^^^^^^^^^^^^^  ^^^^^^^^^^^^^
+%             antecedente          consecuente
+
+% forall(escribio(Artista, Obra), esComic(Obra)). "Para toda obra que escribió X, X es un comic." / "X escribió sólo comics."
+% forall(esComic(Obra), escribio(Artista, Obra)). "Para todo comic, lo escribió X." / "X escribió *todos* los comics."
+
+%puedenSerAmigues(UnaPersona, OtraPersona) :-
+%  esPersona(UnaPersona),
+%  esPersona(OtraPersona),
+%  UnaPersona \= OtraPersona,
+%  forall(esAmigue(UnaPersona, UneAmigue), esAmigue(OtraPersona, UneAmigue)).
+
+soloEscribioComicsOBestSellers(Artista) :-
+  escribio(Artista, _),
+  forall(escribio(Artista, Obra), esComicOBestSeller(Obra)).
+
+esComicOBestSeller(Obra) :-
+  esComic(Obra).
+
+esComicOBestSeller(Obra) :-
+  esBestSeller(Obra).
+
+todasLasObrasSonComics :-
+  forall(escribio(_, Obra), esComic(Obra)).
+% escribio(neilGaiman, sandman), esComic(sandman)
+% escribio(neilGaiman, buenosPresagios), esComic(buenosPresagios)
+% escribio(elsaBornemann, socorro), esComic(socorro)
+% escribio(alanMoore, watchmen), esComic(watchmen)
+% ........
+
+
+% Functores!
+
+% componente(ingrediente, gramos)
+
+
+
+
+esLibroCientifico(UnLibro) :-
+  esDeTipo(UnLibro, cientifico(_)).
+
+
+% novela(Genero, CantidadDeCapitulos)
+% libroDeCuentos(CantidadDeCuentos)
+% libroCientifico(Disciplina)
+% bestSeller(Precio, CantidadDePaginas)
+
+esDeTipo(it, novela(terror, 11)).
+esDeTipo(cuentosDeLaSelva, libroDeCuentos(10)).
+esDeTipo(elUniversoEnUnaTabla, cientifico(quimica)).
+esDeTipo(elUltimoTeoremaDeFermat, cientifico(matematica)).
+esDeTipo(yoRobot, bestSeller(700, 253)).
+
+/*
+estaBuena/1 nos dice cuando una obra está buena. Esto sucede cuando:
+- Es una novela policial y tiene menos de 12 capítulos.
+- Es una novela de terror.
+- Los libros con más de 10 cuentos siempre son buenos.
+- Es una obra científica de fisicaCuantica.
+- Es un best seller y el precio por página es menor a $50.
+*/ 
+
+% Polimorfismo
+
+estaBuena(Obra) :-
+  esDeTipo(Obra, Tipo),
+  esCopado(Tipo).
+
+esCopado(novela(policial, CantidadDeCapitulos)) :-
+  CantidadDeCapitulos < 12.
+
+esCopado(novela(terror, _)).
+
+esCopado(libroDeCuentos(CantidadDeCuentos)) :-
+  CantidadCuentos > 10.
+
+esCopado(libroCientifico(fisicaCuantica)).
+
+esCopado(bestSeller(Precio, CantidadPaginas)) :-
+  (Precio / CantidadPaginas) < 50.
